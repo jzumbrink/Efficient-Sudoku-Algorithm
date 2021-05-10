c47d8456ed2b4cd8a03bb25ed615df690916bbae
Donnerflamme <j.zumbrink@gmx.de>
2021-05-10 23:37:50 +0200

    evaluation first sudoku and introducing idea for variant 2, in addition to current "variant 1"

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ int main() {
     //Felder
     vector<int> fields;
 
-    read_file("../beispiele/1.txt", fields);
+    read_file("../beispiele/2.txt", fields);
 
     int root_n = 3;
     int n = root_n * root_n;
