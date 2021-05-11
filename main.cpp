81ee07348567cf58b8c94787dac26a976c54c20f
Donnerflamme <j.zumbrink@gmx.de>
2021-05-11 09:54:47 +0200

    saving old stuff

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <fstream>
 #include <ctype.h>
+#include <unordered_map>
 
 using namespace std;
 
@@ -30,7 +31,6 @@ void read_file(string filename, vector<int> &fields){
 
     char current_char;
     while (in_file.get(current_char)){
-        cout << current_char << " " << isdigit(current_char) << endl;
         if (isdigit(current_char)){
             fields.push_back(current_char - '0');
         }
@@ -38,17 +38,54 @@ void read_file(string filename, vector<int> &fields){
     in_file.close();
 }
 
-int determine_certain_value(set<int> &horizontal_set, set<int> &vertical_set, set<int> &cluster_set){
+set<int> determine_certain_value_variant_one(set<int> &horizontal_set, set<int> &vertical_set, set<int> &cluster_set){
     //First Intersetion
     set<int> result_step_one;
     set_intersection(horizontal_set.begin(), horizontal_set.end(), vertical_set.begin(), vertical_set.end(), inserter(result_step_one, result_step_one.begin()));
     //Second Intersection with both of first Intersection
     set<int> final_result;
     set_intersection(result_step_one.begin(), result_step_one.end(), cluster_set.begin(), cluster_set.end(), inserter(final_result, final_result.begin()));
+    return final_result;
+}
 
+bool is_value_valid_somewhere_else(vector<int> other_indices, int value){
+
+}
+
+int determine_certain_value_variant_two(set<int> &intersection_result, int horizontal, set<int> &horizontal_set, vector<set<int>> &horizontal_dimensions, int vertical, set<int> &vertical_set, vector<set<int>> &vertical_dimensions, int cluster, set<int> &cluster_set, vector<set<int>> &cluster_dimensions, int index, int root_n){
+    int n = root_n * root_n;
+    set<int> copied_horizontal_set = horizontal_set;
+    int start_horizontal = index - index%n;
+    set<int> copied_vertical_set = vertical_set;
+    set<int> copied_cluster_set = cluster_set;
+    unordered_map<int, int> count_other_possibilities;
+    for (int index_horizontal = start_horizontal; index_horizontal < start_horizontal + n; ++index_horizontal) {
+        //prüfen, ob eine Möglichkeit aus den Sets auf einem Feld platziert werden kann.
+        //Horitontal
+        if (index_horizontal != index){
+            int new_horizontal = get_horizontal(index_horizontal, n);
+            int new_vertical = get_vertical(index_horizontal, n);
+            int new_cluster = get_cluster(index_horizontal, n);
+            set<int> result_values = intersection_three_dimensions(horizontal_dimensions.at(new_horizontal), vertical_dimensions.at(new_vertical), cluster_dimensions.at(new_cluster));
+        }
+    }
+
+
+    set<int>::iterator iterator_pos_value = intersection_result.begin();
+    while (iterator_pos_value != intersection_result.end()){
+
+        iterator_pos_value++;
+    }
+    return 0;
+}
+
+int determine_certain_value(set<int> &horizontal_set, set<int> &vertical_set, set<int> &cluster_set){
+    set<int> final_result = determine_certain_value_variant_one(horizontal_set, vertical_set, cluster_set);
     if(final_result.size() == 1){
+        //1. Variante funktioniert
         return *(final_result.begin());
     } else{
+        //1. Variante funktioniert nicht -> 2. Variante prüfen
         return 0;
     }
 }
@@ -89,12 +126,22 @@ void fill_full_set(set<int> &sample_set, int n){
     }
 }
 
+void fill_dimensions(vector<set<int>> &horizontal_dimensions, vector<set<int>> &vertical_dimensions, vector<set<int>> &cluster_dimensions, int n){
+    set<int> full_set;
+    fill_full_set(full_set, n);
+    for (int i = 0; i < n; ++i) {
+        horizontal_dimensions.push_back(full_set);
+        vertical_dimensions.push_back(full_set);
+        cluster_dimensions.push_back(full_set);
+    }
+}
+
 int main() {
 
     //Felder
     vector<int> fields;
 
-    read_file("../beispiele/2.txt", fields);
+    read_file("../beispiele/1.txt", fields);
 
     int root_n = 3;
     int n = root_n * root_n;
@@ -103,15 +150,7 @@ int main() {
     vector<set<int>> horizontal_dimensions;
     vector<set<int>> vertical_dimensions;
     vector<set<int>> cluster_dimensions;
-
-    set<int> full_set;
-    fill_full_set(full_set, n);
-
-    for (int i = 0; i < n; ++i) {
-        horizontal_dimensions.push_back(full_set);
-        vertical_dimensions.push_back(full_set);
-        cluster_dimensions.push_back(full_set);
-    }
+    fill_dimensions(horizontal_dimensions, vertical_dimensions, cluster_dimensions, n);
 
     queue<int> affected_queue;
     set<int> affected_set;
@@ -119,7 +158,7 @@ int main() {
 
     //Das erste Mal alle offenen Felder der Schlange hinzufügen
     for (int j = 0; j < fields.size(); ++j) {
-        if (fields.at(j)){ //größer als 0
+        if (fields.at(j)){ //größer als 0, vorher festgelegtes Feld
             int value = fields.at(j);
             horizontal = get_horizontal(j, n);
             horizontal_dimensions.at(horizontal).erase(value);
@@ -144,7 +183,7 @@ int main() {
         cluster = get_cluster(index, root_n);
         int determine_result = determine_certain_value(horizontal_dimensions.at(horizontal), vertical_dimensions.at(vertical), cluster_dimensions.at(cluster));
 
-        if (determine_result > 0){
+        if (determine_result > 0){ //oder eine andere Variante ist erfüllt, bisher nur Variante 1 betrachtet -> todo
             //Ein Wert ist eindeutig zuordenbar
             //Wert speichern
             fields.at(index) = determine_result;
@@ -158,6 +197,7 @@ int main() {
         }
     }
 
+    cout << "Result:";
     for (int k = 0; k < fields.size(); ++k) {
         if (k % n == 0){
             cout << endl;
